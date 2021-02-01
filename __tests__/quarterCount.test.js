c2b682dcf0c2ddb1e9114fb529fb66d3a8b1d46c
Jeff Chiu <jwchiu82@gmail.com>
2021-02-01 15:04:32 -0800

    will calculate properly
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- __tests__/quarterCount.test.js
+++ __tests__/quarterCount.test.js
@@ -5,9 +5,28 @@ test('should correctly count the number of quarters for a given dollar amount', 
     expect(quarterCount(amount)).toEqual(10);
 });
 
-test('should correctly count the number of quarters fo a given amount', () => {
-    const amount = 2.66;
-    expect(coinCounter(amount)).toEqual([10]);
+test('should correctly return 0 if amount is equal to 0', () => {
+    const amount = 0;
+    expect(coinCounter(amount)).toEqual([]);
 });
 
+// test('should correctly count the number of quarters fo a given amount', () => {
+//     const amount = 2.66;
+//     expect(coinCounter(amount)).toEqual([10]);
+// });
+
+test('should correctly count the number of quarters and dimes for a given amount', () => {
+    const amount = 2.60;
+    expect(coinCounter(amount)).toEqual([10, 1]);
+});
+
+test('should correctly count number of quarters, dimes and nickles for given amount', () => {
+    const amount = 2.65;
+    expect(coinCounter(amount)).toEqual([10, 1, 1]);
+});
+
+test('should correctly count number of quarters, dimes, nickles and pennies', () => {
+    const amount = 2.67;
+    expect(coinCounter(amount)).toEqual([10,1,1,2]);
+});
 
--- src/js/coinCalc.js
+++ src/js/coinCalc.js
@@ -3,43 +3,26 @@ export function quarterCount(x) {
 }
 export const coinCounter = (amount, array = []) => {
     //termination case
-    if(amount == 0){
-        return amount;
+    if (amount == 0) {
+        return array;
     } else if (amount/.25 >= 1) {
         const q = Math.floor(amount/.25);
         array.push(q)
-        // return coinCounter((amount - q*.25).toFixed(2));
-        return array;
+        return coinCounter((amount - q*.25).toFixed(2), array);
+        // return array
+    } else if (amount/.10 >= 1) {
+        const d = Math.floor(amount/.10);
+        array.push(d);
+        return coinCounter((amount - d*.10).toFixed(2), array);
+    } else if (amount/.05 >= 1) {
+        const n = Math.floor(amount/.05)
+        array.push(n);
+        return coinCounter((amount - n*.05).toFixed(2), array);
+    } else {
+        const p = Math.floor(amount/.01)
+        array.push(p);
+        return coinCounter((amount - p*.01).toFixed(2), array);
     }
 }
 
-// const coinCounter = (amount) => {  e.g. 2.66
-//     const array = []; 
-//     if (amount == 0) { 
-//       return amount; 
-//     }
-//     else if (amount/.25 >= 1) {
-//       const quarters = Math.floor(amount/.25); 2.66/.25 = 10.64 => 10
-//       console.log("quarters: " + quarters); quarters: 10
-//       console.log((amount - quarters*.25).toFixed(2));  2.66 - (10*.25) = .16
-//       return coinCounter((amount - quarters*.25).toFixed(2));
-//     }
-//     else if (amount/.10 >= 1) { 
-//       const dimes = Math.floor(amount/.10);  .16/.10 = 1
-//       console.log("dimes: " + dimes); dimes: 1
-//       console.log((amount-dimes*.10).toFixed(2));  .16 - .1 = .06
-//       return coinCounter((amount-dimes*.10).toFixed(2)); .06
-//     }
-//     else if (amount/.05 >= 1) {  
-//       const nickels = Math.floor(amount/.05); .06/.06 = 1
-//       console.log("nickels: " + nickels); nickles = 1
-//       console.log((amount-nickels*.05).toFixed(2)); .06 - .05 = .01
-//       return coinCounter((amount-nickels*.05).toFixed(2));
-//     }
-//     else if (amount/.01 >= 1) {  
-//       const pennies = Math.floor(amount/.01); .01/.01 = 1
-//       console.log("pennies: " + pennies); pennies: 1
-//       console.log((amount-pennies*.01).toFixed(2));  .01 - (1 * .01) = 0.00
-//       return coinCounter((amount-pennies*.01).toFixed(2));
-//     } 
-//   }
+
